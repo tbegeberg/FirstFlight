ac32fe0b3ee732327f0cf54ac0363589dee13498
Tørk Egeberg <tbegeberg@gmail.com>
2018-01-16 16:32:12 +0100

    Created project

---
+++ FirstFlight/FirstFlight/GameScene.swift
@@ -0,0 +1,17 @@
+//
+//  GameScene.swift
+//  FirstFlight
+//
+//  Created by TørK on 16/01/2018.
+//  Copyright © 2018 Tørk Egeberg. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        self.backgroundColor = SKColor.white
+    }
+}
---
+++ FirstFlight/FirstFlight/GameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GameViewController.swift
+//  FirstFlight
+//
+//  Created by TørK on 16/01/2018.
+//  Copyright © 2018 Tørk Egeberg. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let scene = GameScene(size: view.bounds.size)
+        let skView = view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .resizeFill
+        skView.presentScene(scene)
+    }
+    
+    override var shouldAutorotate: Bool {
+        return true
+    }
+}
